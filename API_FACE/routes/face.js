f823b6fb8f316f8b4b96e301316d2c2a992f2280
BellatiS <bellati.samuele@iisgalvanimi.edu.it>
2020-01-22 11:51:47 +0000

    api

---
+++ API_FACE/routes/face.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+    res.send('respond with a resource');
+});
+
+router.get('/gen', function (req, res, next) {
+    var request = require("request");
+
+    var options = {
+        method: 'POST',
+        url: 'https://faceplusplus-faceplusplus.p.rapidapi.com/facepp/v3/detect',
+        qs: {
+            image_url: 'https://bigironrealtystorage.blob.core.windows.net/prod/2017/02/AV0A3552_2311.jpg'
+        },
+        headers: {
+            'x-rapidapi-host': 'faceplusplus-faceplusplus.p.rapidapi.com',
+            'x-rapidapi-key': '4e8aa3ac44mshf457ad7bc533f67p112053jsn06600d85bc6c',
+            'content-type': 'application/x-www-form-urlencoded'
+        },
+        form: {}
+    };
+
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+
+        console.log(body);
+        res.send(body);
+    });
+
+});
+
+module.exports = router;
